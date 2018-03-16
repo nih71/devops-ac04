a76dcad71ca6e4cef7e1bfc30d2bf62e951b6c6c
Nicolas Silva do Sacramento <36601217+nih71@users.noreply.github.com>
2018-03-16 20:20:41 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -7,7 +7,7 @@ def inicializar():
         tab.append(linha)
     return tab
     
-def maihdhdfsdfshfsdhfsdghdfshdshdfh
+de maihdhdfsdfshfsdhfsdghdfshdshdfh
     jogo = inicializar( )
     print (jogo)
     
