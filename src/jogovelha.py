3b7ca25879f7aa2871aa9273b9f8162946e46737
Nicolas Silva do Sacramento <36601217+nih71@users.noreply.github.com>
2018-03-16 20:29:24 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,15 +1,15 @@
-def inicializar():
-    tab = [ ]
-    for i in range(3):
-        linha = [ ]
-        for j in range(3):
-            linha.append(.")
-        tab.append(linha)
-    return tab
+gfddef inicializar():
+   gfsdg tab = [ ]
+   fdsg for i in range(3):
+     sfdg   linha = [ ]
+     sdfg   for j in range(3):
+    sdfg        linha.append(.")
+     sdfg   tab.append(linha)
+  sfdg  return tab
     
 fsfasdfasd main():
     jogo = inicializar( )
-    print (jogo)
+  sfg  print (jogo)
     
 if __name__ == "__main__":
     main()
